2ef0fd396671fbe3ffa589b129484e3fe252bfc1
ayakade <ayakade13@gmail.com>
2021-02-20 19:50:30 -0800

    deleted MyNewComponent

--- movie-search/src/app/app.module.ts
+++ movie-search/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app-component/app.component';
 import { MagicModule } from './magic/magic.module';
-import { MyNewComponentComponent } from './components/my-new-component/my-new-component.component';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { SearchComponent } from './components/search/search.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent,
     HighlightDirective,
     SearchComponent
   ],
