61b3653912575656a4aa5ade31c2e943bebac28f
ayakade <ayakade13@gmail.com>
2021-02-25 20:07:48 -0800

    update movie detail page

--- movie-search/src/app/components/detail/detail.component.ts
+++ movie-search/src/app/components/detail/detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieapiService } from 'src/app/services/movieapi.service';
+
 
 @Component({
   selector: 'app-detail',
@@ -6,8 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  movie_detail_id: string = 'nothing';
+  movie_detail: any;
+  actors: any;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private movieAPI: MovieapiService) {
+    this.route.params.subscribe( // subscription
+      params => {
+        this.movie_detail_id = params['id'];
+        this.getMovieDetailFromAPICall(this.movie_detail_id);
+      }
+    );
+  }
+
+  getMovieDetailFromAPICall(imdbID:string){
+    // API CALL
+    this.movieAPI.getMovieDetails(this.movie_detail_id)
+    .subscribe(
+      (data:any) =>{
+        this.movie_detail = data;
+        console.log(data);
+        this.actors = data.Actors.split(',');
+    })
+  }
 
   ngOnInit(): void {
   }
